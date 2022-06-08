85e7e36d054616f2c79ef12a6a9215f3b28151d9
Jesus <jesuseduardoperezvargas@gmail.com>
2022-06-08 17:57:11 -0300

    Creada busqueda, funciona ok

--- RestaurantPython/RestaurantPython/urls.py
+++ RestaurantPython/RestaurantPython/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from RestaurantPython.views import index
+from RestaurantPython.views import index, busqueda
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path('bebidas/', include('bebidas.urls')),
     path('comidas/', include('comidas.urls')),
     path('entradas/', include('entradas.urls')),
+    path('busqueda/', busqueda, name = 'busqueda'),
     path('', index, name = 'index')
 ]
--- RestaurantPython/RestaurantPython/views.py
+++ RestaurantPython/RestaurantPython/views.py
@@ -1,18 +1,28 @@
+from multiprocessing import context
 from pipes import Template
 from re import template
 from django.http import HttpResponse
 from django.template import Template, Context
 from django.shortcuts import render
+from entradas.models import Entradas
+from bebidas.models import Bebidas
+from comidas.models import Comidas
 
-
-# def comidas(request):
-#     return render(request, 'familia.html')
+def comidas(request):
+     return render(request, 'familia.html')
 
 def index(request):
     return render(request, 'index.html')
 
-# def bebidas(request):
-#     return render(request, 'productos.html')
+def bebidas(request):
+     return render(request, 'productos.html')
 
 def entradas(request):
     return render(request, 'entradas.html')
+
+def busqueda(request):
+    buscar_entradas = Entradas.objects.filter(nombre__icontains = request.GET['search'])
+    buscar_bebidas = Bebidas.objects.filter(nombre__icontains = request.GET['search'])
+    buscar_comidas = Comidas.objects.filter(nombre__icontains = request.GET['search'])
+    context = {'buscar_entradas':buscar_entradas, 'buscar_bebidas':buscar_bebidas, 'buscar_comidas':buscar_comidas}
+    return render(request, 'busqueda.html', context=context)
