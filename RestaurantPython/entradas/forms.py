50d2354c45a4d31642ffa2bade4359049e35c2a3
Jesus <jesuseduardoperezvargas@gmail.com>
2022-06-08 11:52:19 -0300

    Cambios esteticos

--- RestaurantPython/entradas/forms.py
+++ RestaurantPython/entradas/forms.py
@@ -4,4 +4,4 @@ class Entradas_form(forms.Form):
     nombre = forms.CharField(max_length=30)
     descripcion = forms.CharField(max_length=50)
     precio = forms.FloatField()
-    is_active = forms.BooleanField()
+    activo = forms.BooleanField()
