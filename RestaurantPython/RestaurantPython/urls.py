871476b50fa613f05b605e63fd76a5612bde7177
jorgeapd <jorgeapd5@hotmail.com>
2022-07-04 12:05:29 -0400

    login

--- RestaurantPython/RestaurantPython/urls.py
+++ RestaurantPython/RestaurantPython/urls.py
@@ -1,6 +1,7 @@
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path, include
-from RestaurantPython.views import index, busqueda
+from RestaurantPython.views import index, busqueda, login_view
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,5 +13,6 @@ urlpatterns = [
     path('comidas/', include('comidas.urls')),
     path('entradas/', include('entradas.urls')),
     path('busqueda/', busqueda, name = 'busqueda'),
-    path('', index, name = 'index')
+    path('', index, name = 'index'),
+    path('login/', login_view, name = 'login'),
 ]
